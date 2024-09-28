23993fd99e2f86ec385c5af6f43a99b3fc56ffc3
Amany17Route <gg6229663@gmail.com>
2024-09-29 00:20:58 +0300

    Video 03: UploadFile & Setup Project

---
+++ Company.Service/Helper/DocumentSettings.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Service.Helper
+{
+    public class DocumentSettings
+    {
+        public static string UploadFile(IFormFile file , string folderName)
+        {
+         
+           // var folderPath = @"C:\Users\IT\Desktop\Company.Web\wwwroot\Images\";
+   //1-Get Folder Path
+            var FolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files" ,folderName);
+
+            //2- Get File Name
+            var FileName = $"{Guid.NewGuid()}-{file.FileName}";
+
+            //3- Combine Folder Path with File Name
+            var FilePath =Path.Combine(FolderPath,FileName);
+
+            using var FileStream = new FileStream(FilePath , FileMode.Create);
+
+            file.CopyTo(FileStream);
+
+            return FilePath;
+
+        }
+
+    }
+}

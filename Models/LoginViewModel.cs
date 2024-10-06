29ba93499b01ab965d83891cfee940923732318b
Amany17Route <gg6229663@gmail.com>
2024-10-07 01:06:23 +0300

    Video 03 : Account Controller (SignIn)

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Web.Models
+{
+    public class LoginViewModel
+    {
+
+        [Required(ErrorMessage = "Email Name Is Required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Format")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Password Is Required")]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
